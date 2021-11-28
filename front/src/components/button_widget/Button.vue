64889d89a36e9ef2d8cdfc808c8528ddd9418efc
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2021-11-28 09:45:51 +0700

    #5 create button widget for signin and signup

---
+++ front/src/components/button_widget/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="btn">{{valueButton}}</button>
+</template>
+<script>
+export default {
+    props:['valueButton'],
+}
+</script>
+<style>
+    .btn{
+                padding: 12px;
+                width: 40%;
+                font-size: 15px;
+                font-weight: bold;
+                border: none;
+                outline: none;
+                border-radius: 10px;
+                background: sandybrown;
+                margin-top: 15px;
+                
+            }
+</style>
