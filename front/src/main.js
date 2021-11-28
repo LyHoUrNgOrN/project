f76d17ee0667c41a47db5f9e550765080468a223
sophannachhorn <sophanna.chhorn@student.passerellesnumeriques.org>
2021-11-28 10:54:05 +0700

    #2 Design ERD for whole project

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import SignUp from './components/signUp/SignUp.vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('sign-up', SignUp);
+app.mount('#app');
