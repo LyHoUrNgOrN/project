70b7ce20dea088e71a9d2154e448d6a857559996
sinetsem <73749242+sinetsem@users.noreply.github.com>
2021-11-28 10:23:53 +0700

    Revert "#5 create button widget for signin and signup"

--- front/src/main.js
+++ front/src/main.js
@@ -1,23 +1,4 @@
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
-import App from "./App.vue";
-import BottonWidget from "./components/button_widget/Button.vue";
-import signIn from "./components/signin/Sign-In.vue";
-import signUp from "./components/signUp/signUp.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-app.component("BottonWidget", BottonWidget);
-app.component("signIn", signIn);
-app.component("signUp", signUp);
-
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: '/', component: signIn },
-      { path: '/signIn', component: signIn },
-      { path: '/signUp', component: signUp },
-    ]
-})
-app.use(router);
-app.mount("#app");
+createApp(App).mount('#app')
