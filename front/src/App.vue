057a03cdaba445c7213ecd6edfbc3c94cbb3ebab
sophannachhorn <sophanna.chhorn@student.passerellesnumeriques.org>
2021-12-06 07:55:58 +0700

    #23 create event card

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <event-view></event-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import EventView from './components/UI/EventView.vue'
 export default {
+  components: { EventView },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
