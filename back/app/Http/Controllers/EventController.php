c292de8185bf561cf9c81b40ae9853bbb65bd4a2
somphorsngoun <somphors.ngoun@student.passerellesnumeriques.org>
2021-12-06 19:18:17 +0700

    #20 Manage event on back

---
+++ back/app/Http/Controllers/EventController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAllEvent()
+    {
+        return Event::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function createEvent(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'country' => 'required',
+            'city' => 'required',
+            'description' => 'required',
+            'dateStart' => 'required',
+            'dateEnd' => 'required|after:dateStart',
+        ]);
+        $event = new Event();
+        $event->title = $request->title;
+        $event->dateStart = $request->dateStart;
+        $event->dateEnd = $request->dateEnd;
+        $event->country = $request->country;
+        $event->city = $request->city;
+        $event->description = $request->description;
+        // $event->category_id = $request->category_id;
+
+        $event->save();
+        return response()->json(['message'=> 'Event Created'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function getOneEvent($id)
+    {
+        return Event::findorfail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateEvent(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'country' => 'required',
+            'city' => 'required',
+            'description' => 'required',
+            'dateStart' => 'required',
+            'dateEnd' => 'required|after:dateStart',
+        ]);
+        $event = Event::findorfail($id);
+        $event->title = $request->title;
+        $event->dateStart = $request->dateStart;
+        $event->dateEnd = $request->dateEnd;
+        $event->country = $request->country;
+        $event->city = $request->city;
+        $event->description = $request->description;
+        // $event->category_id = $request->category_id;
+
+        $event->save();
+        return response()->json(['message'=> 'Event Updated'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteEvent($id)
+    {
+        return Event::destroy($id);
+    }
+    
+    /**
+     * search the specified Event from database.
+     *
+     * @param  int  $categoryname
+     * @return \Illuminate\Http\Response
+     */
+    public function searchEvent($title)
+    {
+        return Category::where('title','like','%'.$title.'%')->get();
+    }
+
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('events', [EventController::class, 'getAllEvent']);
+Route::post('events', [EventController::class, 'createEvent']);
+Route::get('events/{id}', [EventController::class, 'getOneEvent']);
+Route::put('events/{id}', [EventController::class, 'updateEvent']);
+Route::delete('events/{id}', [EventController::class, 'deleteEvent']);
+Route::get('/category/search/{title}', [EventController::class, 'searchEvent']);
